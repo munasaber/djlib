c9b1ce3a7740191ef196d03d906627c056a3f97e
deober <derick.ober@gmail.com>
2022-02-10 11:14:38 -0800

    updated documentation for eci_writer

--- clex/clex.py
+++ clex/clex.py
@@ -811,7 +811,7 @@ def plot_eci_uncertainty(eci, title=False):
     return fig
 
 
-def write_eci_json(eci, basis_json_path, output_dict):
+def write_eci_json(eci, basis_json_path):
     """Writes supplied ECI to the eci.json file for use in grand canonical monte carlo. Written for CASM 1.2.0
 
     Parameters:
@@ -822,8 +822,10 @@ def write_eci_json(eci, basis_json_path, output_dict):
     basis_json_path: str
         Path to the casm-generated basis.json file.
 
-    output_path: dict
-        Formatted ECI dictionary. Can be written directly to CASM_root/cluster_expansions/clex.formation_energy/calctype.default/ref.default/bset.default/eci.default
+    Returns:
+    --------
+    data: dict
+        basis.json dictionary formatted with provided eci's
     """
 
     with open(basis_json_path) as f:
