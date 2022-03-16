2f06e3d3c701c64adf7456593f1cf6daf63efa2c
jonnyli <jli5373@gmail.com>
2022-03-16 14:05:49 -0700

    fixed typo

--- djlib.py
+++ djlib.py
@@ -73,7 +73,7 @@ def update_properties_files(casm_root_dir):
                 with open(properties_path) as f:
                     properties = json.load(f)
                 properties["coordinate_mode"] = properties["coord_mode"]
-                if properties["atom_properties"]["force"]["value"]=[]:
+                if properties["atom_properties"]["force"]["value"]==[]:
                     properties["atom_properties"]["force"]["value"]=[[0.0,0.0,0.0]]
                     print("Fixed empty forces in %s" % config)
                 with open(
